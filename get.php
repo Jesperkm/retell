9adf379e3028cce3f6b73a8c2ec6102831b1456a
dyrehauge <dthomasn@gmail.com>
2014-11-19 08:34:53 +0100

    added some style and sort

--- get.php
+++ get.php
@@ -11,10 +11,20 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$result= mysqli_query($conn, "SELECT * FROM product");
+$sort = $_GET["dato"];  
+if ($sort == "asc") {          
+    echo '<p class="info">Showing the elder editions first</p>';
+    $result= mysqli_query($conn, "SELECT * FROM product ORDER BY dato ASC");
 
  while($row = mysqli_fetch_array($result)) {
- echo "<img src='uploads/",$row['img'],"' width='175' height='200' />" . "<br>" . $row['date'] . "<br>" . $row['beskrivelse'] . "<br>";
+ echo "<div class='sized'>" . "<img class='image' src='uploads/",$row['img'],"' width='320px' height='' />" . "<br>" . "<p class='title'>" . $row['beskrivelse'] . "</p>" . "<p class='date'>" . $row['dato'] . "</p>" . "</div>";}      
 }
+else if($sort == "desc") {           
+    echo '<p class="info">Showing the newest editions first</p>';
+    $result= mysqli_query($conn, "SELECT * FROM product ORDER BY dato DESC");
+
+ while($row = mysqli_fetch_array($result)) {
+ echo "<div class='sized'>" . "<img class='image' src='uploads/",$row['img'],"' width='320px' height='' />" . "<br>" . "<p class='title'>" . $row['beskrivelse'] . "</p>" . "<p class='date'>" . $row['dato'] . "</p>" . "</div>";}
+}          
 
 $conn->close();
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>database</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <br>
@@ -15,14 +16,39 @@
             }
         ?>
     </div>
-<form action="#get" method="post" enctype="multipart/form-data">
-    Select image to upload:
+<nav>
+
+    <h1>imageTransfer</h1>
+<form action="#get" method="post" enctype="multipart/form-data" class="right">
+    upload billede:
     <input type="file" name="fileToUpload" id="fileToUpload" class="fileToUpload">
-   <br>BESKRIVELSE <input type="text" name="beskrivelse">
-   <br> <input type="submit" value="Submit" name="submit">
+   BESKRIVELSE <input type="text" name="beskrivelse">
+   <input class="aim" type="submit" value="Tilføj produkt" name="submit">
     <input type="hidden" name="submitted" value="true" />
 </form>
 <br><br>
+</nav>
+
+<div class="sort">
+<form action="" method="get" id="sortit">
+
+               <p>Sort by:</p>
+               <input id="asc" style="display:none;" name="dato" type="radio" value="asc" onclick="document.getElementById('sortit').submit();" />
+               <label for="asc"> <p>Old</p></label>
+               
+               <input id="desc" style="display:none;" name="dato" type="radio" value="desc" onclick="document.getElementById('sortit').submit();"/>
+               <label for="desc"> <p>New</p></label>
+          
+            </form>
+</div>
+<div class="wrap">
+
 <?php include "get.php"; ?>
+</div>
+
+<div id="footer">
+  <p>All Rights reserved</p>
+
+</div>
 </body>
 </html>
